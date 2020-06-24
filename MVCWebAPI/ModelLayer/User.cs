c8e8800bb74e330cd4b1eb9252b9290765c347d9
Gulshan <shrigulshankumar@gmail.com>
2020-06-24 17:01:40 +0300

    Added model properties

--- MVCWebAPI/ModelLayer/User.cs
+++ MVCWebAPI/ModelLayer/User.cs
@@ -2,7 +2,15 @@
 
 namespace ModelLayer
 {
-    public class Class1
+    public class User
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public long ContactNumber { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
     }
 }
